0df5e44eeee6f72906704e87e5110654c38be449
Felipe <felipe.pereira.dev@gmail.com>
2020-04-03 22:30:52 -0300

    game over

--- script.js
+++ script.js
@@ -37,18 +37,26 @@ function update(event) {
 }
 
 function startGame() {
+
   if (snake[0].x > 15 * box && direction == 'right') snake[0].x = 0
   if (snake[0].x < 0 && direction == 'left') snake[0].x = 16 * box
   if (snake[0].y > 15 * box && direction == 'down') snake[0].y = 0
   if (snake[0].y < 0 && direction == 'up') snake[0].y = 16 * box
 
+  for(i = 1; i < snake.length; i++){
+    if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+      clearInterval(jogo)
+      alert('Game over! :(')
+    }
+  }
+
   createBG()
   drawSnake()
   drawFood()
-
+  
   let snakeX = snake[0].x
   let snakeY = snake[0].y
-
+  
   if (direction == 'right') snakeX += box
   if (direction == 'left') snakeX -= box
   if (direction == 'up') snakeY -= box
