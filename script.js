98a4471fef33ae9da0f4f44d97904c3cbd873a59
Felipe <felipe.pereira.dev@gmail.com>
2020-04-03 21:32:44 -0300

    moves

--- script.js
+++ script.js
@@ -6,17 +6,37 @@ snake[0] = {
   x: 8 * box,
   y: 8 * box
 }
+let direction = 'right'
 
-function criarBG(){
+function createBG(){
   context.fillStyle = 'lightgreen'
   context.fillRect(0, 0, 16 * box, 16 * box)
 }
-function criarCobrinha(){
+function createSnake(){
   for(let i = 0; i< snake.length; i++){
     context.fillStyle = 'green'
     context.fillRect(snake[i].x, snake[i].y, box, box)
   }
 }
 
-criarBG()
-criarCobrinha()
+function startGame(){
+  createBG()
+  createSnake()
+
+  let snakeX = snake[0].x
+  let snakeY = snake[0].y
+
+  if(direction=='right') snakeX +=box
+  if(direction=='left') snakeX -=box
+  if(direction=='up') snakeY -=box
+  if(direction=='down') snakeX +=box
+
+  snake.pop()
+
+  snake.unshift({
+    x: snakeX,
+    y: snakeY
+  })
+}
+
+let jogo = setInterval(startGame, 100)
